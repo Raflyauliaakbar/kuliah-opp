14a43e1047a422fe2b9346fef87cd115adfe2715
Raflyauliaakbar <mraflyauliaakbar03@gmail.com>
2024-03-22 11:11:21 +0800

    Memodifikasi Hello.java

--- Hello.java
+++ Hello.java
@@ -1,6 +1,6 @@
 public class Hello{
  public static void main (String [] args){
-  System.out.println("Hello Git !!");
-  System.out.println("Hello Java !!");
+ String nama = "M.Rafly Aulia Akbar";
+  System.out.println("Hello " +nama);
  }
 }
